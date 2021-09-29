43f1cde98f7ded3c34214ca7ee445e3b5d447f37
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-09-29 20:13:04 +0200

    Create getUsers copy.php

---
+++ users-match-backend/getUsers copy.php
@@ -0,0 +1,17 @@
+<?php
+    if($_GET['action'] == 'getuserlist') {
+        // Read the JSON file from the root folder of the website
+        $jsonFile = file_get_contents("userDatabase.json");
+        echo $jsonFile;
+        /*
+            Properties of the userDB class:
+            -id
+            -firstname
+            -lastname
+            -age
+            -haircolor
+            -countryName
+            -gender
+        */
+    }
+?>
