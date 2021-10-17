1cc0cbf2897c458f5bb9bea909f98a881305e924
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-10-17 11:30:02 +0200

    Project templates - update

---
+++ file-upload/index.php
@@ -0,0 +1,8 @@
+<center>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:<br>
+        <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+        <input type="hidden" name="action" value="upload">
+        <input type="submit" value="Upload File" name="submit">
+    </form>
+</center>
