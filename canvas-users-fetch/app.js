e9f55d6b2fbb0eec61219cd8aa2ea4fb92c07775
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-10-18 13:24:04 +0200

    canvas-users-fetch

---
+++ canvas-users-fetch/app.js
@@ -0,0 +1,31 @@
+// Vanilla JS ❤️ 🧡 💛 💚 💙 💜 
+
+async function getUsers() {
+    const response = await fetch("https://cederdorff.github.io/web-frontend/canvas-users/data.json");
+    const data = await response.json();
+    console.log(data.slice(0, 5));
+    return data;
+}
+
+function appendUsers(users) {
+    let htmlTemplate = "";
+    for (const user of users) {
+        console.log(user);
+        htmlTemplate += /*html*/`
+            <article>
+                <img src="${user.avatar_url}">
+                <h2>${user.name}</h2>
+                <a href="mailto:${user.email}">${user.email}</a>
+                <p>Role: ${user.enrollment_type}</p>
+            </article>
+        `;
+    }
+    document.querySelector("#users").innerHTML = htmlTemplate;
+}
+
+async function initApp() {
+    const users = await getUsers();
+    appendUsers(users);
+}
+
+initApp();
