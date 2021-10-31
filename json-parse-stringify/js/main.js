0c9d945388bfbc45989c0c5b279016d482b98386
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-10-31 14:42:47 +0100

    new project added

---
+++ json-parse-stringify/js/main.js
@@ -0,0 +1,18 @@
+
+const user = {
+  name: "John",
+  age: 30,
+  gender: "male",
+  lookingFor: "female"
+};
+
+// === JSON.stringify === //
+const jsonUser = JSON.stringify(user);
+console.log(jsonUser); // {"name":"John","age":30,"gender":"male","lookingFor":"female"}
+
+// === JSON.parse === //
+const jsonString = '{"name":"John","age":30,"gender":"male","lookingFor":"female"}';
+const userObject = JSON.parse(jsonString);
+console.log(userObject); // logging userObject
+
+
