2e5c5f42e9902e26376b47fe0b1aff0281133942
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-11-07 22:13:43 +0100

    Update create.js

--- dating-spa-modules/src/pages/create.js
+++ dating-spa-modules/src/pages/create.js
@@ -1,8 +1,18 @@
 class CreatePage {
     constructor() {
-        this.id = "create";
-        this.render();
+        this.id = "create"; this.render();
 
+        this.firstnameInput = document.querySelector(`#${this.id} [name="firstname"]`);
+        this.lastnameInput = document.querySelector(`#${this.id} [name="lastname"]`);
+        this.ageInput = document.querySelector(`#${this.id} [name="age"]`);
+        this.haircolorInput = document.querySelector(`#${this.id} [name="haircolor"]`);
+        this.countryInput = document.querySelector(`#${this.id} [name="country"]`);
+        this.genderInput = document.querySelector(`#${this.id} [name="gender"]`);
+        this.lookingForInput = document.querySelector(`#${this.id} [name="lookingFor"]`);
+        this.imagePreview = document.querySelector(`#${this.id} [name="imagePreview"]`);
+        this.imageInput = document.querySelector(`#${this.id} [name="profileImage"]`);
+
+        this.attachEvents();
     }
 
     render() {
@@ -14,31 +24,45 @@ class CreatePage {
                 </header>
                 <section>
                     <form>
-                        <input type="text" name="firstname" id="firstname" placeholder="Firstname">
-                        <input type="text" name="lastname" id="lastname" placeholder="Lastname">
-                        <input type="number" name="age" id="age" placeholder="Age">
-                        <input type="text" name="haircolor" id="haircolor" placeholder="Hair Color">
-                        <input type="text" name="country" id="country" placeholder="Country">
-                        <select name="gender" id="gender">
-                        <option value="" selected disabled>Select gender</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Binary">Binary</option>
+                        <input type="text" name="firstname" placeholder="Firstname">
+                        <input type="text" name="lastname" placeholder="Lastname">
+                        <input type="number" name="age" placeholder="Age">
+                        <input type="text" name="haircolor" placeholder="Hair Color">
+                        <input type="text" name="country" placeholder="Country">
+                        <select name="gender">
+                            <option value="" selected disabled>Select gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Binary">Binary</option>
                         </select>
-                        <select name="lookingFor" id="lookingFor">
-                        <option value="" selected disabled>Select looking for</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Binary">Binary</option>
+                        <select name="lookingFor">
+                            <option value="" selected disabled>Select looking for</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Binary">Binary</option>
                         </select>
-                        <img id="imagePreview" class="image-preview">
-                        <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*"
-                        onchange="previewImage(this.files[0], 'imagePreview')">
-                        <button type="button" onclick="createUserEvent()">Save</button>
+                        <img name="imagePreview" class="image-preview">
+                        <input type="file" name="profileImage" accept="image/*">
+                        <button type="button" class="save">Save</button>
                     </form>
                 </section>
             </section>
         `);
+    }
+
+    attachEvents() {
+        this.imageInput.onchange = () => this.previewImage();
+    }
+
+    previewImage() {
+        const file = this.imageInput.files[0];
+        if (file) {
+            let reader = new FileReader();
+            reader.onload = event => {
+                this.imagePreview.setAttribute('src', event.target.result);
+            };
+            reader.readAsDataURL(file);
+        }
     }
 
     beforeShow(params) {
