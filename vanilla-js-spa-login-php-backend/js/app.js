d4f8ae692e1779332467b2e8e56e4a749117e808
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-12-02 13:58:39 +0100

    Update app.js

--- vanilla-js-spa-login-php-backend/js/app.js
+++ vanilla-js-spa-login-php-backend/js/app.js
@@ -1,19 +1,6 @@
 import { navigateTo } from "./router.js";
 console.log("app.js is running!");
 
-// window.login = () => {
-//     const mail = document.querySelector("#login-mail").value;
-//     const password = document.querySelector("#login-password").value;
-
-//     if (mail === "race@eaaa.dk" && password === "test01") {
-//         localStorage.setItem("userIsAuthenticated", true);
-//         document.querySelector(".login-message").innerHTML = "";
-//         navigateTo("#/");
-//     } else {
-//         document.querySelector(".login-message").innerHTML = "User not found. Wrong mail og password.";
-//     }
-// }
-
 async function login() {
 	const username = document.querySelector("#login-username").value;
 	const password = document.querySelector("#login-password").value;
