1b59ac1cd18375e862ab79306e5ddc90d38dc3fc
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-06-23 16:29:42 +0200

    project added

---
+++ class-person-basic/js/main.js
@@ -0,0 +1,50 @@
+class Person {
+  constructor(name, mail, birthDate, img) {
+    this.name = name;
+    this.mail = mail;
+    this.birthDate = birthDate;
+    this.img = img;
+  }
+
+  log() {
+    console.log(`
+      Name: ${this.name}, 
+      Mail: ${this.mail}, 
+      Birth date: ${this.birthDate}, 
+      Image Url: ${this.img}
+    `);
+  }
+
+  getAge() {
+    const birthDate = new Date(this.birthDate);
+    const today = new Date();
+    const diff = new Date(today - birthDate);
+    return diff.getFullYear() - 1970;
+  }
+
+  getHtmlTemplate() {
+    const template = /*html*/`
+      <article>
+        <img src="${this.img}">
+        <h2>${this.name}</h2>
+        <a href="mailto:${this.mail}">${this.mail}</a>
+        <p>Birth date: ${this.birthDate}</p>
+        <p>Age: ${this.getAge()} years old</p>
+      </article>
+    `;
+
+    return template;
+  }
+}
+
+const persons = [
+  new Person("Birgitte", "bki@mail.dk", "1966-01-14", "https://www.eaaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg"),
+  new Person("Martin", "mnor@mail.dk", "1989-05-02", "https://www.eaaa.dk/media/oayjq02h/martin-n%C3%B8hr.jpg"),
+  new Person("Rasmus", "race@mail.dk", "1990-09-15", "https://www.eaaa.dk/media/devlvvgj/rasmus-cederdorff.jpg")
+];
+
+console.log(persons);
+
+for (const person of persons) {
+  document.querySelector("#content").innerHTML += person.getHtmlTemplate();
+}
