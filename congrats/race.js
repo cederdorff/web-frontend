40d6804e741dc943c223e5f66fc2c09c67fcd50c
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-01-23 19:29:02 +0100

    race app updated

--- congrats/race.js
+++ congrats/race.js
@@ -1,3 +1,16 @@
-const race = {};
+class Race {
+    says(toClass, congrats) {
+        console.log(`
+            Congratulations to ${toClass.name}, ${toClass.description} 
+            ${congrats.message}
+        `);
+    }
+
+    codeEveryDay() {
+        console.log("Code Every Day!");
+    }
+}
+
+const race = new Race();
 
 export default race;
